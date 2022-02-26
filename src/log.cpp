684a2f78e520ed7820e9b85fad21ec99039057b5
Karl Ljungkvist <k.ljungkvist@gmail.com>
2022-02-26 14:27:40 +0100

    inline the log calls

--- src/log.cpp
+++ src/log.cpp
@@ -1,13 +1,11 @@
 #include "log.h"
-#include <fmt/core.h>
 
 static bool log_enabled{false};
 
 void log_set_enable(bool enabled) {
     log_enabled = enabled;
 }
-void log(const std::string &s) {
-    if(log_enabled) {
-        fmt::print(s);
-    }
+
+bool log_get_enable() {
+    return log_enabled;
 }
--- src/log.h
+++ src/log.h
@@ -2,8 +2,15 @@
 #define LOG_H
 
 #include <string>
+#include <fmt/core.h>
 
 void log_set_enable(bool enabled);
-void log(const std::string &s);
+bool log_get_enable();
+
+static inline void log(const std::string &s) {
+    if(log_get_enable()) {
+        fmt::print(s);
+    }
+}
 
 #endif /* LOG_H */
